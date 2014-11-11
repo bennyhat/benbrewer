af41f53f8a728d1a02b3954e5d7e95e7837a3896
Benjamin Brewer <BenBrewer@dswinc.com>
2014-11-11 09:17:38 -0500

    changed to use PhiloGL

--- js/application.js
+++ js/application.js
@@ -1,25 +1,98 @@
-var glDefaultContext;
-var tjsScene;
-var tjsCamera;
-var tjsRenderer;
+function initializeWebGL(sCanvasId) {
+    var joCanvas = $("#" + sCanvasId);
+    joCanvas.attr("width", joCanvas.innerWidth());
+    joCanvas.attr("height", joCanvas.innerHeight());
 
-function initializeWebGL(joCanvas) {
-    tjsScene = new THREE.Scene();
-    tjsCamera = new THREE.PerspectiveCamera(200, joCanvas.innerWidth() / joCanvas.innerHeight(), 1, 100);
-    tjsRenderer = new THREE.WebGLRenderer();
+//    var cube = new PhiloGL.O3D.Cube({
+//        colors: [1, 1, 1, 1]
+//    });
 
-    var tjgCube = new THREE.BoxGeometry(10, 10, 10);
-    var tjmCube = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    var tjmeCube = new THREE.Mesh(tjgCube, tjmCube);
+    var plane = new PhiloGL.O3D.Plane({
+        type: "x,z",
+        xlen: 8.5,
+        zlen: 8.5,
+        nx: 1,
+        nz: 1,
+        offset: 0,
+        colors: [1, 0, 0, 1]
+    });
 
-    tjsScene.add(tjmeCube);
-    tjsRenderer.setSize(joCanvas.innerWidth(), joCanvas.innerHeight());
-    joCanvas[0].appendChild(tjsRenderer.domElement);
+    PhiloGL(sCanvasId, {
+        program: {
+            from: 'ids',
+            vs: 'shader-vs',
+            fs: 'shader-fs'
+        },
+        onError: function () {
+            alert("An error ocurred while loading the application");
+        },
+        onLoad: function (app) {
+            var gl = app.gl,
+                canvas = app.canvas,
+                program = app.program,
+                camera = app.camera,
+                view = new PhiloGL.Mat4,
+                rPyramid = 0, rCube = 0;
 
-    tjsRenderer.render(tjsScene, tjsCamera);
+            gl.viewport(0, 0, canvas.width, canvas.height);
+            gl.clearColor(1, 1, 1, 1);
+            gl.clearDepth(1);
+            gl.enable(gl.DEPTH_TEST);
+            gl.depthFunc(gl.LEQUAL);
+
+            camera.view.id();
+
+            function setupElement(elem) {
+                //update element matrix
+                elem.update();
+                //get new view matrix out of element and camera matrices
+                view.mulMat42(camera.view, elem.matrix);
+                //set buffers with element data
+                program.setBuffers({
+                    'aVertexPosition': {
+                        value: elem.vertices,
+                        size: 3
+                    },
+                    'aVertexColor': {
+                        value: elem.colors,
+                        size: 4
+                    }
+                });
+                //set uniforms
+                program.setUniform('uMVMatrix', view);
+                program.setUniform('uPMatrix', camera.projection);
+            }
+
+            function animate() {
+                rPyramid += 0.01;
+                rCube += 0.01;
+            }
+
+            function tick() {
+                drawScene();
+                PhiloGL.Fx.requestAnimationFrame(tick);
+            }
+
+            function drawScene() {
+                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+                //Draw Cube
+                plane.position.set(0, 0, -10);
+                plane.rotation.set(.25, 0, 0);
+                setupElement(plane);
+                program.setBuffer('indices', {
+                    value: plane.indices,
+                    bufferType: gl.ELEMENT_ARRAY_BUFFER,
+                    size: 1
+                });
+                gl.drawElements(gl.TRIANGLES, plane.indices.length, gl.UNSIGNED_SHORT, 0);
+            }
+
+            tick();
+        }
+    });
 }
 
 $(document).ready(function () {
-    var canvas = $("#viewport");
-    glDefaultContext = initializeWebGL(canvas);
+    initializeWebGL("viewport");
 });
