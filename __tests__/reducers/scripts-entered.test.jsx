2bd23e627136c46a90ab5e163924fde6292340e7
Ben Brewer <ben@benbrewer.me>
2017-05-14 12:51:49 -0400

    add a reducer that preprocesses input for continuations (trailing backlash or unbalanced quotes)

---
+++ __tests__/reducers/scripts-entered.test.jsx
@@ -0,0 +1,195 @@
+import {inputAction} from "../../src/actions";
+import scriptEnteredReducer from "../../src/reducers/script-entered.jsx";
+
+describe('ScriptEnteredReducer', () => {
+  let state = {
+    inputHistory: ['something there already'],
+    newInputPresent: false
+  };
+
+  describe('given a simple script', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction('simple script');
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('adds the simple script to the input history array, and indicates new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already',
+          'simple script'
+        ],
+        newInputPresent: true
+      });
+    });
+  });
+  describe('given a script with an unterminated double quote', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction('simple script"');
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with a unterminated single quote', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script'");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with an escape in the last position', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction('simple script\\');
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with a double quote that is escaped', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction('simple script\\\\\\"');
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('adds the script to the input history array, and indicates new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already',
+          'simple script\\\\\\"' // this is really simple script\\\"
+        ],
+        newInputPresent: true
+      });
+    });
+  });
+  describe('given a script with a single quote that is escaped', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\\\\\\'");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('adds the script to the input history array, and indicates new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already',
+          "simple script\\\\\\'" // this is really simple script\\\'
+        ],
+        newInputPresent: true
+      });
+    });
+  });
+  describe('given a trailing escape that is escaped', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\\\\");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('adds the script to the input history array, and indicates new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already',
+          "simple script\\\\" // this is really simple script\\
+        ],
+        newInputPresent: true
+      });
+    });
+  });
+  describe('given a script with a double quote that is part of a larger escape', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction('simple script\\\\\\\\"');
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with a single quote that is part of a larger escape', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\\\\\\\\'");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with an ending backlash that is part of a larger escape', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\\\\\\");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with a backlash in the last position for multiple lines', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\nnext line\\");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('does not add anything to the input history, and does not indicate new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already'
+        ],
+        newInputPresent: false
+      });
+    });
+  });
+  describe('given a script with a backlash at the end of a line, but not at the last position for multiple lines', () => {
+    let newState = {};
+    beforeEach(() => {
+      let simpleScriptInputAction = inputAction("simple script\\\nnext line");
+      newState = scriptEnteredReducer(state, simpleScriptInputAction);
+    });
+    it('adds the script to the input history array, and indicates new input', () => {
+      expect(newState).toEqual({
+        inputHistory: [
+          'something there already',
+          'simple script\\\nnext line'
+        ],
+        newInputPresent: true
+      });
+    });
+  });
+});
---
+++ src/reducers/script-entered.jsx
@@ -0,0 +1,31 @@
+function scriptStillOpen(input) {
+  let cleanedInput = input.replace(/\\\\/g, '');
+  return unbalancedQuotesPresent(cleanedInput) || trailingBackslashPresent(cleanedInput);
+}
+
+function unbalancedQuotesPresent(cleanedInput) {
+  return (cleanedInput.match(/(?:[^\\]|^)['"]/g) || []).length % 2 !== 0;
+}
+function trailingBackslashPresent(cleanedInput) {
+  return (cleanedInput.match(/(?:[^\\]|^)\\$/g) || []).length > 0
+}
+
+const scriptEnteredReducer = (state, action) => {
+  const input = action.value;
+
+  let updatedState = {};
+  if (scriptStillOpen(input)) {
+    updatedState = Object.assign({}, state, {
+      newInputPresent: false
+    });
+  }
+  else {
+    updatedState = Object.assign({}, state, {
+      inputHistory: state.inputHistory.concat(input),
+      newInputPresent: true
+    });
+  }
+  return updatedState;
+};
+
+export default scriptEnteredReducer;
